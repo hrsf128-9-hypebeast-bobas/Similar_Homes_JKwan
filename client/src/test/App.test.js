99f1f9387c9626df61737cf1cf6259c3b7dc9921
Justin Kwan <jkwan19@gmail.com>
2020-06-24 22:11:05 -0700

    Added unit tests

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,5 +1,6 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount, render } from 'enzyme'
 import App from '../Components/App.jsx'
+import axios from 'axios'
 
 describe('App Component', () => {
   const wrapper = shallow(<App />);
@@ -8,10 +9,16 @@ describe('App Component', () => {
     expect(wrapper.type()).toEqual('div');
   });
 
-  it('Should check componentDidMount()', () => {
-    const instance = wrapper.instance();
-    jest.spyOn(instance, 'componentDidMount');
-    instance.componentDidMount();
-    expect(instance.randomFunction).toHaveBeenCalledTimes(1);
+  it('Should call componentDidMount', () => {
+    jest.spyOn(App.prototype, 'componentDidMount');
+    const wrapper = shallow(<App />);
+    expect(App.prototype.componentDidMount.mock.calls.length).toBe(1);
   })
+
+  it('Should fetch a list of listings', () => {
+    const getSpy = jest.spyOn(axios, 'get')
+    const wrapper = shallow(<App />)
+    expect(getSpy).toBeCalled()
+  })
+
 });
---
+++ client/src/test/NearbyHomes.test.js
@@ -0,0 +1,23 @@
+import { shallow, mount, render } from 'enzyme';
+import App from '../Components/App.jsx'
+import NearbyHomes from '../Components/NearbyHomes.jsx'
+
+
+describe('NearbyHomes Component', () => {
+  const wrapper = mount(<App />);
+
+  it('Should expect NearbyHomes to exist', () => {
+    expect(wrapper.find(NearbyHomes).exists()).toEqual(true)
+  });
+
+  it('Should render header', () => {
+    expect(wrapper.find('h2').exists()).toEqual(true)
+  })
+  it('Should expect an image to render', () => {
+    expect(wrapper.find('img')).toHaveLength(1);
+  })
+
+  it('Should expect the property descriptions to render', () => {
+    expect(wrapper.exists('carousel-caption')).toEqual(true);
+  })
+})
