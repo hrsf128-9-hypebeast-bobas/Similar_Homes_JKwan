7e1593ced46df99209aae726c32cf00a2fed5429
Justin Kwan <jkwan19@gmail.com>
2020-06-28 22:23:40 -0700

    Edits to button effects

--- client/src/Components/SimilarHomesPrevious.jsx
+++ client/src/Components/SimilarHomesPrevious.jsx
@@ -6,7 +6,7 @@ const SimilarHomesPrevious = (props) => {
     <div className={styles.previousSlider}>
       <button onClick={props.previous} type="button" className={styles.sliderController}>
         <div className={styles.SVGContainer}>
-          <svg className={styles.svg} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M14.292 16.494l7.147 7.056-1.869 1.893-9.067-8.951 9.069-8.927 1.866 1.896z" fill="#869099"></path>
+          <svg className={styles.svg} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path  d="M14.292 16.494l7.147 7.056-1.869 1.893-9.067-8.951 9.069-8.927 1.866 1.896z" fill="#869099"></path>
           </svg>
         </div>
       </button>
