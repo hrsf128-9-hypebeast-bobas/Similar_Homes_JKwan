940e8f85b5bbb1bd8c2c2363cc6bebb9e6f368d3
Eliza Drinker <elizaleedrinker@gmail.com>
2020-07-20 08:06:01 -0700

    Minimal testing for csv helper functions

--- database/__tests__/helpers.test.js
+++ database/__tests__/helpers.test.js
@@ -1,4 +1,7 @@
-const { generateName, generateNeighborhoodName, generateCityName, generateStreetNames, shuffle } = require('../csvSeeding/helpers');
+const {
+  generateName, generateNeighborhoodName,
+  generateCityName, generateStreetNames,
+} = require('../csvSeeding/helpers');
 
 describe('select random below', () => {
   test('', () => {
@@ -13,26 +16,8 @@ describe('generate name function', () => {
       names.push(generateName());
     }
     const name = names[0];
-    console.log(names);
+    // console.log(names);
     expect(typeof name).toBe('string');
-  });
-});
-
-describe('shuffle', () => {
-  test('shuffles array', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7];
-    const shuffled = shuffle([...arr]);
-    expect(shuffled).not.toEqual(arr);
-    expect(shuffled.length).toBe(arr.length);
-  });
-});
-
-describe('shuffle', () => {
-  test('shuffles array', () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7];
-    const shuffled = shuffle([...arr]);
-    expect(shuffled).not.toEqual(arr);
-    expect(shuffled.length).toBe(arr.length);
   });
 });
 
@@ -43,7 +28,7 @@ describe('generate neighborhood name', () => {
       names.push(generateNeighborhoodName());
     }
     const name = names[0];
-    console.log('Neighborhood names', names);
+    // console.log('Neighborhood names', names);
     expect(typeof name).toBe('string');
   });
 });
@@ -55,7 +40,7 @@ describe('generate city name', () => {
       names.push(generateCityName());
     }
     const name = names[0];
-    console.log('City names', names);
+    // console.log('City names', names);
     expect(typeof name).toBe('string');
   });
 });
@@ -64,7 +49,7 @@ describe('generate street names', () => {
   test('returns a string', () => {
     const names = generateStreetNames(20);
     const name = names[0];
-    console.log('Street names', names);
+    // console.log('Street names', names);
     expect(typeof name).toBe('string');
   });
 });
---
+++ database/__tests__/random.test.js
@@ -0,0 +1,21 @@
+const { shuffle, distributedRandomInt } = require('../csvSeeding/random');
+
+describe('shuffle', () => {
+  test('shuffles array', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    const shuffled = shuffle([...arr]);
+    expect(shuffled).not.toEqual(arr);
+    expect(shuffled.length).toBe(arr.length);
+  });
+});
+
+// can test by actually running a bunch of times I guess? and checking.... mean...?
+describe('distributed skewed randoms', () => {
+  test('returns number in range', () => {
+    const min = 0;
+    const max = 5;
+    const rand = distributedRandomInt(0, 5);
+    expect(rand).toBeLessThan(max);
+    expect(rand).toBeGreaterThan(min);
+  });
+});
