5ca5bb17f276f6f2291cb08e2000381bec009f37
Eliza Drinker <elizaleedrinker@gmail.com>
2020-07-21 22:02:02 -0700

    Edit CSVWriter to split files
    Edit CSVWriter to split files after reaching certain row
    count

--- database/csvSeeding/lib/CsvWriter.js
+++ database/csvSeeding/lib/CsvWriter.js
@@ -7,22 +7,37 @@ const path = require('path');
 // \\\\\\\\\\\\\\\\\\ //
 
 const CsvWriter = function CsvWriter(filepath, fields, timerMessage) {
-  if (timerMessage) {
-    console.time(timerMessage);
-  }
   this.paused = false;
   this.filepath = path.join(__dirname, filepath);
+  this.timerMessage = timerMessage;
+  this.maxRows = 100000;
+  this.rowCount = 0;
+  this.filesProduced = 0;
   // append to file instead of overwriting (usef)
-  this.stream = fs.createWriteStream(this.filepath, 'utf8', { flags: 'a' })
+  this.stream = this.newStream();
+  this.fields = fields;
+};
+
+CsvWriter.prototype.currentFilepath = function currentFilepath() {
+  return this.filepath + this.filesProduced;
+};
+
+CsvWriter.prototype.newStream = function newStream() {
+  if (this.timerMessage) {
+    console.time(this.timerMessage + this.filesProduced);
+  }
+  const stream = fs.createWriteStream(this.currentFilepath(), 'utf8', { flags: 'a' })
     .on('error', () => {
-      console.log(`Csv write error with ${timerMessage || filepath}`);
+      console.log(`Csv write error with ${this.timerMessage + this.filesProduced || this.filepath}`);
     })
     .on('finish', () => {
-      if (timerMessage) {
-        console.timeEnd(timerMessage);
+      if (this.timerMessage) {
+        console.timeEnd(this.timerMessage + this.filesProduced);
       }
     });
-  this.writeHeader(fields);
+  // write header
+  stream.write(this.fields);
+  return stream;
 };
 
 CsvWriter.prototype.toCsvRow = function toCsvRow(array) {
@@ -30,29 +45,30 @@ CsvWriter.prototype.toCsvRow = function toCsvRow(array) {
 };
 
 CsvWriter.prototype.writeRow = function writeRow(row) {
-  return this.stream.write(this.toCsvRow(row));
+  if (this.rowCount > this.rowLimit) {
+    this.stream.end();
+    this.rowCount = 0;
+    this.paused = false;
+    this.filesProduced += 1;
+    this.stream = this.newStream();
+  }
+  const isWritable = this.stream.write(this.toCsvRow(row));
+  this.paused = !isWritable;
+  this.rowCount += 1;
 };
 
-CsvWriter.prototype.writeHeader = function writeHeader(fields) {
-  this.writeRow(fields);
-};
-
-CsvWriter.prototype.end = function writeHeader() {
+CsvWriter.prototype.end = function end() {
   this.stream.end();
 };
 
-CsvWriter.prototype.processRowsWithDrain = function processRowsWithDrain(processor) {
-  const rowProcessor = function rowProcessor(row) {
-    const isWritable = this.writeRow(row);
-    this.paused = !isWritable;
-  };
+CsvWriter.prototype.writeRowsWithDrain = function writeRowsWithDrain(rowGenerator) {
   const continueCondition = function continueCondition() { return !this.paused; };
 
-  processor(rowProcessor, continueCondition);
+  rowGenerator(this.writeRow, continueCondition);
   if (this.paused) {
     this.stream.once('drain', () => {
       this.paused = false;
-      this.processAndDrain(processor);
+      this.writeRowsWithDrain(rowGenerator);
     });
   }
 };
